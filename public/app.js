a51aa62d2caccc7de77fc94d5ffa38cde8385ee5
Juliana Gomez <juliana@gomezjuliana.com>
2017-08-30 10:56:11 -0500

    add loop to get all the people

--- public/app.js
+++ public/app.js
@@ -1,26 +1,28 @@
 fetch('/data/data.json')
 .then(response => response.json())
-.then(createCard).catch(() => console.log('Oops! Something went wrong'))
+.then(createCards).catch(() => console.log('Oops! Something went wrong'))
 
-function createCard(data){
-	const rootDiv = document.querySelector('.root');
+function createCards(data){
+	for (let x = 0; x<data.people.length; x++){
+		const rootDiv = document.querySelector('.root');
+		const containerDiv = document.createElement('div');
+		rootDiv.appendChild(containerDiv);
 
-	const heading = document.createElement('h1');
-	const name = document.createTextNode(data.people[0].name);
-	heading.appendChild(name);
-	rootDiv.appendChild(heading); 
+		containerDiv.style.backgroundImage = `url(${data.people[x].photo})`;
 
-	const subtitle = document.createElement('h5');
-	const text = document.createTextNode(data.people[0].date+' in '+data.people[0].category);
-	subtitle.appendChild(text);
-	rootDiv.appendChild(subtitle);
+		const heading = document.createElement('h1');
+		const name = document.createTextNode(data.people[x].name);
+		heading.appendChild(name);
+		containerDiv.appendChild(heading); 
 
-	const blurb = document.createElement('p');
-	const blurbText = document.createTextNode(data.people[0].blurb);
-	blurb.appendChild(blurbText);
-	rootDiv.appendChild(blurb);
+		const subtitle = document.createElement('h5');
+		const text = document.createTextNode(data.people[x].date+' in '+data.people[x].category);
+		subtitle.appendChild(text);
+		containerDiv.appendChild(subtitle);
 
-	const img = document.createElement('img');
-	img.src = data.people[0].photo;
-	rootDiv.appendChild(img);
+		const blurb = document.createElement('p');
+		const blurbText = document.createTextNode(data.people[x].blurb);
+		blurb.appendChild(blurbText);
+		containerDiv.appendChild(blurb);
+	}
 }
