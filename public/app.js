a716fc100d293d882d90a003b84e4e42b6768144
Juliana Gomez <juliana@gomezjuliana.com>
2017-08-31 14:15:21 -0500

    create form dynamically, work on vote function

--- public/app.js
+++ public/app.js
@@ -1,63 +1,96 @@
-// fetch('/data/data.json')
-// .then(response => response.json())
-// .then(createCards).catch(() => console.log('Oops! Something went wrong'))
+fetch('/data/data.json')
+.then(response => response.json())
+.then(createCards).catch(() => console.log('Oops! Something went wrong'))
 
-// function createCards(data){
-// 	for (let x = 0; x<data.people.length; x++){
-// 		const rootDiv = document.querySelector('.root');
-// 		const containerDiv = document.createElement('div');
-// 		rootDiv.appendChild(containerDiv);
+function createCards(data){
+	for (let x = 0; x<data.people.length; x++){
+		const rootDiv = document.querySelector('.root');
+		const containerDiv = document.createElement('div');
+		rootDiv.appendChild(containerDiv);
 
-// 		containerDiv.style.backgroundImage = `url(${data.people[x].photo})`;
+		containerDiv.style.backgroundImage = `url(${data.people[x].photo})`;
 
-// 		const heading = document.createElement('h1');
-// 		const name = document.createTextNode(data.people[x].name);
-// 		heading.appendChild(name);
-// 		containerDiv.appendChild(heading); 
+		const heading = document.createElement('h1');
+		const name = document.createTextNode(data.people[x].name);
+		heading.appendChild(name);
+		containerDiv.appendChild(heading); 
 
-// 		const subtitle = document.createElement('h5');
-// 		const text = document.createTextNode(data.people[x].date+' in '+data.people[x].category);
-// 		subtitle.appendChild(text);
-// 		containerDiv.appendChild(subtitle);
+		const subtitle = document.createElement('h5');
+		const text = document.createTextNode(data.people[x].date+' in '+data.people[x].category);
+		subtitle.appendChild(text);
+		containerDiv.appendChild(subtitle);
 
-// 		const blurb = document.createElement('p');
-// 		const blurbText = document.createTextNode(data.people[x].blurb);
-// 		blurb.appendChild(blurbText);
-// 		containerDiv.appendChild(blurb);
+		const blurb = document.createElement('p');
+		const blurbText = document.createTextNode(data.people[x].blurb);
+		blurb.appendChild(blurbText);
+		containerDiv.appendChild(blurb);
 
-// 		const vote = createVotingButtons();
-// 		containerDiv.appendChild(vote)
-// 	}
-// }
+		const vote = createVotingForm();
+		containerDiv.appendChild(vote)
 
-// function createVotingButtons() {
-// 	const div = document.createElement('div');
+		const counterDiv = document.createElement('div');
+		counterDiv.classList.add('counter');
+		containerDiv.appendChild(counterDiv);
 
-// 	const upVote = document.createElement('img');
-// 	upVote.src = "https://d30y9cdsu7xlg0.cloudfront.net/png/100266-200.png";
-// 	upVote.addEventListener('click', (e) => console.log(e.target));
-// 	div.appendChild(upVote);
+		const yesBar = document.createElement('div');
+		yesBar.classList.add('yes');
+		counterDiv.appendChild(yesBar);
 
-// 	const downVote = document.createElement('img');
-// 	downVote.src= "https://d30y9cdsu7xlg0.cloudfront.net/png/70800-200.png";
-// 	div.appendChild(downVote);
+		const noBar = document.createElement('div');
+		noBar.classList.add('no');
+		counterDiv.appendChild(noBar);
+	}
+}
 
-// 	const button = document.createElement('button');
-// 	const label = document.createTextNode('Vote Now');
-// 	button.appendChild(label);
-// 	div.appendChild(button);
-// 	return div;
-// }
+function createVotingForm(){
+	const form = document.createElement('form');
+	form.classList.add('form');
 
-const form = document.querySelector('.form');
-form.addEventListener('submit', voteCalc);
+	//yes input
+	const yesLabel = document.createElement('label');
+	yesLabel.setAttribute('for', 'yes');
+	form.appendChild(yesLabel);
 
-let totalVotes = 0;
-let upVotes = 0;
-let downVotes = 0;
+	const yesInput = document.createElement('input');
+	yesInput.setAttribute('type', 'radio');
+	yesInput.setAttribute('name', 'vote');
+	yesInput.id = "yes";
+	yesLabel.appendChild(yesInput);
 
-function voteCalc(e){
+	const yesSpan = document.createElement('span');
+	yesLabel.appendChild(yesSpan);
+
+	//no input
+	const noLabel = document.createElement('label');
+	noLabel.setAttribute('for', 'no');
+	form.appendChild(noLabel);
+
+	const noInput = document.createElement('input');
+	noInput.setAttribute('type', 'radio');
+	noInput.setAttribute('name', 'vote');
+	noInput.id = "no";
+	noLabel.appendChild(noInput);
+
+	const noSpan = document.createElement('span');
+	noLabel.appendChild(noSpan);
+
+	//button
+	const button = document.createElement('input');
+	button.setAttribute('type', 'submit');
+	button.setAttribute('name', 'vote');
+	form.appendChild(button);
+
+	let totalVotes = 0;
+	let upVotes = 0;
+	let downVotes = 0;
+
+	form.addEventListener('submit', (e) => voteCalc(e, totalVotes, upVotes, downVotes));
+	return form;
+}
+
+function voteCalc(e, totalVotes, upVotes, downVotes){
 	e.preventDefault();
+
 	const upVote = document.getElementById('yes');
 
 	const yesBar = document.querySelector('.yes');
@@ -74,7 +107,6 @@ function voteCalc(e){
 		noBar.style.width = (downVotes/totalVotes) * 100+"%";
 		yesBar.style.width = 100 -((downVotes/totalVotes) * 100)+"%";
 	}
+
+	console.log(totalVotes, upVotes, downVotes);
 }
-
-
-
