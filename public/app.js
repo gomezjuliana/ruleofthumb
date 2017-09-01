02689352c5968c7b9d08869da2e4755ac115c3fe
Juliana Gomez <juliana@gomezjuliana.com>
2017-08-31 20:31:29 -0500

    fix voting function

--- public/app.js
+++ public/app.js
@@ -6,31 +6,33 @@ function createCards(data){
 	for (let x = 0; x<data.people.length; x++){
 		const rootDiv = document.querySelector('.root');
 		const containerDiv = document.createElement('div');
+		containerDiv.classList.add('containerDiv');
 		rootDiv.appendChild(containerDiv);
 
 		containerDiv.style.backgroundImage = `url(${data.people[x].photo})`;
 
+		const contentDiv = document.createElement('div');
+		contentDiv.classList.add('contentDiv');
+		containerDiv.appendChild(contentDiv);
+
 		const heading = document.createElement('h1');
 		const name = document.createTextNode(data.people[x].name);
 		heading.appendChild(name);
-		containerDiv.appendChild(heading); 
+		contentDiv.appendChild(heading); 
 
 		const subtitle = document.createElement('h5');
 		const text = document.createTextNode(data.people[x].date+' in '+data.people[x].category);
 		subtitle.appendChild(text);
-		containerDiv.appendChild(subtitle);
+		contentDiv.appendChild(subtitle);
 
 		const blurb = document.createElement('p');
 		const blurbText = document.createTextNode(data.people[x].blurb);
 		blurb.appendChild(blurbText);
-		containerDiv.appendChild(blurb);
+		contentDiv.appendChild(blurb);
 
-		const vote = createVotingForm();
-		containerDiv.appendChild(vote)
 
 		const counterDiv = document.createElement('div');
 		counterDiv.classList.add('counter');
-		containerDiv.appendChild(counterDiv);
 
 		const yesBar = document.createElement('div');
 		yesBar.classList.add('yes');
@@ -39,22 +41,49 @@ function createCards(data){
 		const noBar = document.createElement('div');
 		noBar.classList.add('no');
 		counterDiv.appendChild(noBar);
+		
+		const vote = createVotingForm(counterDiv, x);
+		contentDiv.appendChild(vote);
+
+		contentDiv.appendChild(counterDiv);
 	}
 }
 
-function createVotingForm(){
+function createVotingForm(counterDiv, x){
+	function voteCalc(e){
+		e.preventDefault();
+
+		const upVote = form.querySelector('.input-yes');
+
+		const yesBar = counterDiv.querySelector('.yes');
+		const noBar = counterDiv.querySelector('.no');
+
+		if (upVote.checked){
+			++upVotes
+		} else {
+			++downVotes
+		}
+		const totalVotes = upVotes + downVotes;
+
+		yesBar.style.width = (upVotes/totalVotes) * 100+"%";
+		noBar.style.width = 100 - ((upVotes/totalVotes) * 100)+"%";
+
+		console.log(totalVotes, upVotes, downVotes);
+	}
+
 	const form = document.createElement('form');
 	form.classList.add('form');
 
 	//yes input
 	const yesLabel = document.createElement('label');
-	yesLabel.setAttribute('for', 'yes');
+	yesLabel.setAttribute('for', `yes-${x}`);
 	form.appendChild(yesLabel);
 
 	const yesInput = document.createElement('input');
 	yesInput.setAttribute('type', 'radio');
 	yesInput.setAttribute('name', 'vote');
-	yesInput.id = "yes";
+	yesInput.id = `yes-${x}`;
+	yesInput.classList.add('input-yes');
 	yesLabel.appendChild(yesInput);
 
 	const yesSpan = document.createElement('span');
@@ -62,13 +91,13 @@ function createVotingForm(){
 
 	//no input
 	const noLabel = document.createElement('label');
-	noLabel.setAttribute('for', 'no');
+	noLabel.setAttribute('for', `no-${x}`);
 	form.appendChild(noLabel);
 
 	const noInput = document.createElement('input');
 	noInput.setAttribute('type', 'radio');
 	noInput.setAttribute('name', 'vote');
-	noInput.id = "no";
+	noInput.id = `no-${x}`
 	noLabel.appendChild(noInput);
 
 	const noSpan = document.createElement('span');
@@ -80,33 +109,10 @@ function createVotingForm(){
 	button.setAttribute('name', 'vote');
 	form.appendChild(button);
 
-	let totalVotes = 0;
 	let upVotes = 0;
 	let downVotes = 0;
 
-	form.addEventListener('submit', (e) => voteCalc(e, totalVotes, upVotes, downVotes));
+	form.addEventListener('submit', voteCalc);
+
 	return form;
-}
-
-function voteCalc(e, totalVotes, upVotes, downVotes){
-	e.preventDefault();
-
-	const upVote = document.getElementById('yes');
-
-	const yesBar = document.querySelector('.yes');
-	const noBar = document.querySelector('.no');
-
-	++totalVotes
-
-	if (upVote.checked){
-		++upVotes
-		yesBar.style.width = (upVotes/totalVotes) * 100+"%";
-		noBar.style.width = 100 - ((upVotes/totalVotes) * 100)+"%";
-	} else {
-		++downVotes
-		noBar.style.width = (downVotes/totalVotes) * 100+"%";
-		yesBar.style.width = 100 -((downVotes/totalVotes) * 100)+"%";
-	}
-
-	console.log(totalVotes, upVotes, downVotes);
 }
